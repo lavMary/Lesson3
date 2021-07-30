2c75d3055cf1db1adcc90141e1a3f05f4ce1bcb8
lavMary <49761493+lavMary@users.noreply.github.com>
2021-07-30 14:15:44 +0300

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i=5; i>0;i--) {
+            System.out.println(i);
+        }
+    }
+}
