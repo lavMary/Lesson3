6fd907e51a8fa59c5dd0b0f8a6564c246b206c51
lavMary <49761493+lavMary@users.noreply.github.com>
2021-08-04 22:18:17 +0300

    Update 1.java

--- 1.java
+++ 1.java
@@ -5,7 +5,7 @@ import java.sql.SQLOutput;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i=5; i>0;i--) {
+        for (int i = 5; i > 0; i--) {
             System.out.println(i);
         }
     }
