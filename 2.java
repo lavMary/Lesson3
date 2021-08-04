c0e238ee1f8fb510e0cd4bf4c43091e24f8b7783
lavMary <49761493+lavMary@users.noreply.github.com>
2021-08-04 22:19:27 +0300

    Update 2.java

--- 2.java
+++ 2.java
@@ -7,11 +7,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int c=0;
-        for (int i=1; i <= n; i++) {
-            c += i;
+        int inpNum = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= inpNnum; i++) {
+            sum += i;
         }
-        System.out.println(c);
+        System.out.println(sum)
     }
 }
